bab2a8058374d7b204bdaafb25f65f4038ad5892
xoxowo <1qq11212@gmail.com>
2022-06-15 01:55:42 +0900

    06/15 index3 및 js 이미지 슬라이드 기능 추가.

--- main2.js
+++ main2.js
@@ -1,14 +1,42 @@
-const content = "내가 좋아하는 것들"
-const text = document.querySelector(".text")
 
-let index = 0;
+let curPos = 0; // 현재 보여주는 이미지의 인덱스 번호
+let positionValue = 0; // 이미지 태그의 위치 값 지정할 변수
+const IMAGE_WIDTH = 300;
 
-function typing() {
-    text.textContent += content[index++];
-    if (index > content.length){
-        text.textContent = ""
-        index = 0;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const images = document.querySelector(".images");
+
+function next(){
+    if(curPos < 2){
+        prevBtn.removeAttribute('disabled')
+        positionValue -= IMAGE_WIDTH;
+        images.style.transform = `translateX(${positionValue}px)`;
+        curPos += 1;
+    }
+    if(curPos === 2){
+        nextBtn.setAttribute('disabled', 'true'); 
     }
 }
 
-setInterval(typing, 280)
+function prev(){
+    if(curPos > 0){
+        nextBtn.removeAttribute('disabled')
+        positionValue += IMAGE_WIDTH;
+        images.style.transform = `translateX(${positionValue}px)`;
+        curPos -= 1;
+    }
+    if(curPos === 0){
+        prevBtn.setAttribute('disabled', 'true'); 
+    }
+}
+
+
+
+function init(){
+    prevBtn.setAttribute('disabled', 'true');  // 첫 번째 사진이 앞에 있을 때 앞버튼 못누르게
+    prevBtn.addEventListener("click", prev);
+    nextBtn.addEventListener("click", next);
+};
+
+init();
